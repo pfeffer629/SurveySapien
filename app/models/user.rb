adad43e9ce1a9a7c16aa42a82ad60f9c41c3e0ff
Mark R <mark.francis.reid@gmail.com>
2014-12-18 12:02:24 -0800

    added gravatar column migration and method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_many :surveys
   validates :email, :username, {presence: true, uniqueness: true}
   validates :pw_hash, presence: true
+
+  before_save :gravatar_src
+
+  def gravatar_src
+    email_address = self.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    self.gravatar = "http://www.gravatar.com/avatar/#{hash}"
+  end
+
 end

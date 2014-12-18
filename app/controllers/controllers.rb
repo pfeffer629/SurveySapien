bf590c793f70782d8e59d6e324f94e91658a9278
Mark Reid <mark.francis.reid@gmail.com>
2014-12-18 15:35:01 -0800

    added user auth

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -1,3 +1,28 @@
 get '/' do
-  erb :create_survey
+	if session[:user_id]
+  		erb :create_survey
+  	else
+  		erb :login
+  	end
+end
+
+post '/signup' do
+  user = User.new(username: params[:username], pw_hash: params[:password], email: params[:email])
+  if user.save
+    session[:user_id]=user.id
+  end
+  redirect '/'
+end
+
+post '/login' do
+  @user = User.find_by_email(params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  end
+end
+
+get '/logout' do
+  session[:user_id]=nil
+  redirect '/'
 end

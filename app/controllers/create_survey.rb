68a4a64552b8ca5b66ad9b5662ce9d5ab3354964
Mark R <mark.francis.reid@gmail.com>
2014-12-18 17:12:50 -0800

    AJAXified delete button

--- app/controllers/create_survey.rb
+++ app/controllers/create_survey.rb
@@ -6,9 +6,6 @@ post '/surveys' do
   # FIXME: This makes survey w/ placeholder user_id; needs to be update to a real user id
   survey = Survey.new(title: params[:title], user_id: 1)
 
-  p "HERE WE ARE THFDJKSHFJDKSFHDS"
-  p params[:questions]
-
   if survey.save
     params[:questions].each do |question|
       Question.create(query: question, survey_id: survey.id)
@@ -19,4 +16,11 @@ post '/surveys' do
   end
 
   redirect to "/surveys/#{survey.id}"
+end
+
+delete '/surveys/:id' do
+  survey = Survey.find(params[:id])
+  survey.destroy
+  content_type :json
+  {id: params[:id]}.to_json
 end

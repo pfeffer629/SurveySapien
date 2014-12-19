8a6261f081a7ff1d6fb9dcb018895d5402ae771a
Tina Tien & Vincent Colavin <pair+ttien28.vcolavin@devbootcamp.com>
2014-12-18 16:49:53 -0800

    simplified submission creation DOM so that AJAX can mess with it

--- app/controllers/create_survey.rb
+++ app/controllers/create_survey.rb
@@ -6,8 +6,11 @@ post '/surveys' do
   # FIXME: This makes survey w/ placeholder user_id; needs to be update to a real user id
   survey = Survey.new(title: params[:title], user_id: 1)
 
+  p "HERE WE ARE THFDJKSHFJDKSFHDS"
+  p params[:questions]
+
   if survey.save
-    params[:questions].each do |_,question|
+    params[:questions].each do |question|
       Question.create(query: question, survey_id: survey.id)
     end
   else

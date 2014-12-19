8fe6b86a760fd47f283716af7a7588fdd3e544f9
Tina Tien & Vincent Colavin <pair+ttien28.vcolavin@devbootcamp.com>
2014-12-18 18:49:00 -0800

    this isn't actually AJAX but it works

--- public/js/application.js
+++ public/js/application.js
@@ -13,11 +13,19 @@ $(document).ready(function() {
   $('.remove_question').on('click', function(event){
     event.preventDefault();
     $(".question").last().remove();
+    $("li").last().remove();
   })
 
+  $('.add_question').on('click', function(event){
+    event.preventDefault();
+    var lastListItem = $
+    $('<li>').appendTo('.question_list')
+    $('<input>').attr({
+      class: "question",
+      type: "text",
+      name: "questions[]",
+      placeholder: "type question here"
+    }).appendTo('li:last-child')
+  })
 
-  // $('.add_question').on('click', function(event){
-  //   event.preventDefault();
-  //   $()
-  // })
 });
